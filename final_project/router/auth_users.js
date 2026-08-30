8945b0a0383de1e1f9fc60de459e68bcff37d9e4
loredanatibu <loredanatibu@yahoo.com>
2026-08-30 13:44:46 -0400

    wip

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -3,7 +3,32 @@ const jwt = require('jsonwebtoken');
 let books = require("./booksdb.js");
 const regd_users = express.Router();
 
-let users = [];
+let users = [
+    {
+        username:"user_1",
+        password:"passw_1",
+        firstName: "John",
+        lastName: "wick",
+        email:"johnwick@gmail.com",
+        DOB:"22-01-1990",
+    },
+    {
+        username:"user_2",
+        password:"passw_2",
+        firstName: "John",
+        lastName: "smith",
+        email:"johnsmith@gmail.com",
+        DOB:"21-07-1983",
+    },
+    {
+        username:"user_3",
+        password:"passw_3",
+        firstName: "Joyal",
+        lastName: "white",
+        email:"joyalwhite@gmsil.com",
+        DOB:"21-03-1989",
+    },
+];
 
 const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -8,7 +8,20 @@ const public_users = express.Router();
 // lsof -ti :5000 | xargs kill -9
 public_users.post("/register", (req,res) => {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+
+  const { username, password } = req.body;
+  if (!username || !password) {
+    return res.status(400).json({ message: "Username and password are required" });
+  }
+
+  const userExists = users.find(u => u.username === username);
+  if (userExists) {
+    return res.status(409).json({ message: "Username already exists" });
+  }
+
+  users.push({ username, password });
+  return res.status(200).json({ message: "User successfully registered" });
 });
 
 // Get the book list available in the shop
